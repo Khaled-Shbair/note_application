034a9f17c7bfa65f1020ae9e6bf947e6c6ab0d23
Khaled <khaled.shbair12@gmail.com>
2023-08-14 21:20:10 +0300

    edit manager_sizes.dart

--- lib/core/resources/manager_sizes.dart
+++ lib/core/resources/manager_sizes.dart
@@ -1,15 +1,31 @@
 import '../util/size_util.dart';
 
 class ManagerHeight {
+  static double h6 = SizeUtil.setHeight(6);
   static double h20 = SizeUtil.setHeight(20);
+  static double h24 = SizeUtil.setHeight(24);
+  static double h32 = SizeUtil.setHeight(32);
+  static double h56 = SizeUtil.setHeight(56);
+  static double h60 = SizeUtil.setHeight(60);
+  static double h62 = SizeUtil.setHeight(62);
+  static double h90 = SizeUtil.setHeight(90);
+  static double h398 = SizeUtil.setHeight(398);
 }
 
 class ManagerWidth {
-  static double w20 = SizeUtil.setWidth(20);
+  static double w2 = SizeUtil.setWidth(2);
+  static double w10 = SizeUtil.setWidth(10);
+  static double w24 = SizeUtil.setWidth(24);
+  static double w26 = SizeUtil.setWidth(26);
+  static double w34 = SizeUtil.setWidth(34);
+  static double w70 = SizeUtil.setWidth(70);
 }
 
 class ManagerRadius {
+  static double r14 = SizeUtil.setRadius(14);
   static double r16 = SizeUtil.setRadius(16);
+  static double r20 = SizeUtil.setRadius(20);
+  static double r46 = SizeUtil.setRadius(46);
 }
 
 class ManagerIconSize {
