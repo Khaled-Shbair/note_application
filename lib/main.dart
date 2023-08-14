386a2a3d3480b0476c5c5cb5c994d1cfaca90a19
Khaled <khaled.shbair12@gmail.com>
2023-08-14 14:05:33 +0300

    edit on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,34 @@
-import 'package:flutter/material.dart';
+import 'core/service/theme_service.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:flutter/material.dart';
 import 'config/constants.dart';
+import 'routes/route_generator.dart';
+import 'routes/routes.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key}) : _themeService = ThemeService();
+
+  final ThemeService _themeService;
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: Size(Constants.designSizeWidth, Constants.designSizeHeight),
-        minTextAdapt: true,
-        splitScreenMode: true,
+      designSize: Size(Constants.designSizeWidth, Constants.designSizeHeight),
+      minTextAdapt: true,
+      splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.splashScreen,
+          theme: _themeService.getThemeData(),
+          themeMode: _themeService.getThemeMode(),
         );
-      }
+      },
     );
   }
 }
