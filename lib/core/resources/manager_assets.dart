aa0bf2c2d038cce7672d3051b9235253d6baf131
Khaled <khaled.shbair12@gmail.com>
2023-08-14 21:17:16 +0300

    edit manager_assets.dart

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -1,12 +1,7 @@
 const String _imagePath = 'assets/images/';
 
 class ManagerAssets {
-  static const String logo = '${_imagePath}logo.svg';
-
-  // OnBoarding screen
   static const String imageOnBoardingOne = '${_imagePath}onBoardingOne.svg';
-  static const String imageOnBoardingTwo = '${_imagePath}onBoardingTwo.svg';
+  static const String imageOnBoardingTwo = '${_imagePath}onBoardingTow.svg';
   static const String imageOnBoardingThree = '${_imagePath}onBoardingThree.svg';
-
-// screen
 }
