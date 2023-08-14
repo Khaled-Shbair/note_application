6e3490531c5136e8e60ecedddaa9337a92684909
Khaled <khaled.shbair12@gmail.com>
2023-08-14 21:12:57 +0300

    add some variables in Constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,17 @@
 class Constants {
-  static double designSizeWidth = 360;
-  static double designSizeHeight = 690;
-  static double elevationAppBar = 0;
-
+  static const double designSizeWidth = 390;
+  static const double designSizeHeight = 844;
+  static const double elevationAppBar = 0;
+  static const double blurRadiusOfBoxShadowToBottomContainerInOnBoardingScreen =
+      2.73;
+  static const double
+      spreadRadiusOfBoxShadowToBottomContainerInOnBoardingScreen = 0;
+  static const double xOffsetOfBoxShadow1ToBottomContainerInOnBoardingScreen =
+      -0.91;
+  static const double yOffsetOfBoxShadow1ToBottomContainerInOnBoardingScreen =
+      -0.91;
+  static const double xOffsetOfBoxShadow2ToBottomContainerInOnBoardingScreen =
+  1.37;
+  static const double yOffsetOfBoxShadow2ToBottomContainerInOnBoardingScreen =
+  1.37;
 }
