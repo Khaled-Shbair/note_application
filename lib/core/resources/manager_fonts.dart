1ea0df4e41e33a2c49b1ddffeaf097a519b81216
Khaled <khaled.shbair12@gmail.com>
2023-08-14 21:19:55 +0300

    edit manager_fonts.dart

--- lib/core/resources/manager_fonts.dart
+++ lib/core/resources/manager_fonts.dart
@@ -5,16 +5,16 @@ class ManagerFontSize {
   static double s12 = SizeUtil.setFontSize(12);
   static double s14 = SizeUtil.setFontSize(14);
   static double s18 = SizeUtil.setFontSize(18);
+  static double s32 = SizeUtil.setFontSize(32);
 }
 
 class ManagerFontWeight {
-  static const FontWeight w300 = FontWeight.w300;
   static const FontWeight w400 = FontWeight.w400;
   static const FontWeight w500 = FontWeight.w500;
+  static const FontWeight w600 = FontWeight.w600;
   static const FontWeight w700 = FontWeight.w700;
-  static const FontWeight w800 = FontWeight.w800;
 }
 
 class ManagerFontFamily {
-  // static const String almarai = '';
+  static const String nunitoSans = 'Nunito Sans';
 }
