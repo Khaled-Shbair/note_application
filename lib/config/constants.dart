69a62d82ccc236b4837500d7324f38e51d67f41f
Khaled <khaled.shbair12@gmail.com>
2023-08-15 01:39:58 +0300

    add sqflite, path_provider and path libraries and create tables database

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -12,7 +12,25 @@ class Constants {
   static const double yOffsetOfBoxShadow1ToBottomContainerInOnBoardingScreen =
       -0.91;
   static const double xOffsetOfBoxShadow2ToBottomContainerInOnBoardingScreen =
-  1.37;
+      1.37;
   static const double yOffsetOfBoxShadow2ToBottomContainerInOnBoardingScreen =
-  1.37;
+      1.37;
+
+  /// Constants Database properties
+  static const String databaseName = 'db.sql';
+  static int versionDatabase = 1;
+
+  // 1- Note table
+  static const String noteTableName = 'notes';
+  static const String idNoteColumn = 'id';
+  static const String contentNoteColumn = 'content';
+  static const String dateNoteColumn = 'date';
+  static const String timeNoteColumn = 'time';
+  static const String colorNoteColumn = 'color';
+
+  // 2- Image table
+  static const String imageTableName = 'images';
+  static const String idImageColumn = 'id';
+  static const String noteIdImageColumn = 'id_note';
+  static const String pathImageColumn = 'path_image';
 }
