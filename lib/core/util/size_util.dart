75e549595c609727771cd84e05cc1b0919536c78
Khaled <khaled.shbair12@gmail.com>
2023-08-14 13:39:25 +0300

    add flutter_screenutil library and create size_util.dart

---
+++ lib/core/util/size_util.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SizeUtil {
+  static double setWidth(double width) {
+    return width.w;
+  }
+
+  static double setHeight(double height) {
+    return height.h;
+  }
+
+  static double setFontSize(double fontSize) {
+    return fontSize.sp;
+  }
+
+  static double setRadius(double radius) {
+    return radius.r;
+  }
+}
