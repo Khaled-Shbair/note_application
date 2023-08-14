ef064bd55d95b99e1dcaa86dcbf35e9ff50f40a0
Khaled <khaled.shbair12@gmail.com>
2023-08-14 14:16:29 +0300

    add route onBoarding screen

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -1,9 +1,15 @@
 import '../core/resources/manager_strings.dart';
 import 'package:flutter/material.dart';
 
+import '../features/on_boarding/presentation/view/screens/on_boarding_screen.dart';
+import 'routes.dart';
+
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings settings) {
     switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+
       default:
         return unDefinedRoute();
     }
