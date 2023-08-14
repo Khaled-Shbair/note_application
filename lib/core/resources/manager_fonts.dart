ee0053e2c10c37283dbcbd885530b9871b1cecbd
Khaled <khaled.shbair12@gmail.com>
2023-08-14 13:45:56 +0300

    create manager_fonts.dart

---
+++ lib/core/resources/manager_fonts.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../util/size_util.dart';
+
+class ManagerFontSize {
+  static double s12 = SizeUtil.setFontSize(12);
+  static double s14 = SizeUtil.setFontSize(14);
+  static double s18 = SizeUtil.setFontSize(18);
+}
+
+class ManagerFontWeight {
+  static const FontWeight w300 = FontWeight.w300;
+  static const FontWeight w400 = FontWeight.w400;
+  static const FontWeight w500 = FontWeight.w500;
+  static const FontWeight w700 = FontWeight.w700;
+  static const FontWeight w800 = FontWeight.w800;
+}
+
+class ManagerFontFamily {
+  // static const String almarai = '';
+}
