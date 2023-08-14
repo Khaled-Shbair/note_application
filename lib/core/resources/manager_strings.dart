2566f897e55ef80edd7891dba647a70d6afe6652
Khaled <khaled.shbair12@gmail.com>
2023-08-14 13:45:33 +0300

    create manager_strings.dart to manage all strings of application

---
+++ lib/core/resources/manager_strings.dart
@@ -0,0 +1,12 @@
+class ManagerStrings {
+  static const String noRouteFound = 'No route found';
+  static const String titleOnBoardingOne = 'Manage your notes easily';
+  static const String titleOnBoardingTwo = 'Organize your thoughts';
+  static const String titleOnBoardingThree = 'Create cards and easy styling';
+  static const String subTitleOnBoardingOne =
+      'A completely easy way to manage and customize your notes.';
+  static const String subTitleOnBoardingTwo =
+      'Most beautiful note taking application.';
+  static const String subTitleOnBoardingThree =
+      'Making your content legible has never been easier.';
+}
