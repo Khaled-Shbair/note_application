f8e9eb561ad40eb6bec44849ca10bd3a7b6788f6
Khaled <khaled.shbair12@gmail.com>
2023-08-14 14:00:44 +0300

    create manager_dark_theme.dart

---
+++ lib/core/theme/manager_dark_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../config/constants.dart';
+import '../resources/manager_colors.dart';
+import '../resources/manager_fonts.dart';
+import '../resources/manager_styles.dart';
+import '../resources/manager_text_theme_dark.dart';
+
+ThemeData managerDarkThemeData() {
+  return ThemeData.dark().copyWith(
+    useMaterial3: true,
+    primaryColor: ManagerColors.primaryColor,
+    primaryColorDark: ManagerColors.primaryColorDark,
+    scaffoldBackgroundColor: ManagerColors.scaffoldBackgroundColorDark,
+    textTheme: ManagerTextThemeDark(),
+    appBarTheme: AppBarTheme(
+      backgroundColor: ManagerColors.appBarBackgroundColorDark,
+      elevation: Constants.elevationAppBar,
+      centerTitle: true,
+      titleTextStyle: boldTextStyle(
+        color: ManagerColors.black,
+        fontSize: ManagerFontSize.s18,
+        fontFamily: ManagerFontFamily.almarai,
+      ),
+    ),
+    // this for disabled button color
+    disabledColor: ManagerColors.disabledColorDark,
+  );
+}
