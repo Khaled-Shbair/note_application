6323eedf87c6d43a7b4f8d743bb7a5c96fb19804
Khaled <khaled.shbair12@gmail.com>
2023-08-14 13:46:07 +0300

    create manager_colors.dart

---
+++ lib/core/resources/manager_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static const Color primaryColor = Colors.white;
+  static const Color primaryColorLight = Colors.white;
+  static const Color primaryColorDark = Colors.black;
+  static const Color scaffoldBackgroundColorLight = Colors.white;
+  static const Color scaffoldBackgroundColorDark = Colors.black;
+  static const Color appBarBackgroundColorLight = Colors.white;
+  static const Color appBarBackgroundColorDark = Colors.white;
+  static const Color disabledColorLight = Colors.white;
+  static const Color disabledColorDark = Colors.white;
+}
