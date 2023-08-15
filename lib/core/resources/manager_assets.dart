df82d0729b369c3c02050b25739081f45e4389b3
Khaled <khaled.shbair12@gmail.com>
2023-08-15 21:56:23 +0300

    add paths of images icons

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -4,4 +4,8 @@ class ManagerAssets {
   static const String imageOnBoardingOne = '${_imagePath}onBoardingOne.png';
   static const String imageOnBoardingTwo = '${_imagePath}onBoardingTow.png';
   static const String imageOnBoardingThree = '${_imagePath}onBoardingThree.png';
+  static const String allNotesIcon = '${_imagePath}all_notes_icon.png';
+  static const String noteIcon = '${_imagePath}note_icon.svg';
+  static const String hiddenIcon = '${_imagePath}hidden_icon.svg';
+  static const String favouriteIcon = '${_imagePath}favourite_icon.svg';
 }
