6263d53bcc29ec856ee29077263fafdfecd61dc6
Khaled <khaled.shbair12@gmail.com>
2023-08-15 21:59:45 +0300

    create AllNotesScreen

---
+++ lib/features/note/presentation/view/screens/all_notes_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AllNotesScreen extends StatelessWidget {
+  const AllNotesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
