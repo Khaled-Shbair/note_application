232e97eeb443a9b726e82542da1bdf16529d3e0e
Khaled <khaled.shbair12@gmail.com>
2023-08-15 21:57:51 +0300

    add new color

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -6,11 +6,13 @@ class ManagerColors {
   static const Color greyLightColor = Color(0XFF979797);
   static const Color whiteLightColor = Color(0XFFF9F9F9);
   static const Color textColor = Color(0XFF1C2121);
+  static Color textGreyColor = const Color(0XFF3C3C43).withOpacity(0.60);
   static Color shadowColor = const Color(0XFFAEAEC0).withOpacity(.4);
+  static Color shadowColors = const Color(0XFFAEAEC0).withOpacity(.15);
   static const Color primaryColorLight = Colors.white;
   static const Color primaryColorDark = Colors.black;
-  static const Color scaffoldBackgroundColorLight = Colors.white;
-  static const Color appBarBackgroundColorLight = Colors.white;
+  static const Color scaffoldBackgroundColorLight = Color(0XFFF2F2F6);
+  static const Color appBarBackgroundColorLight = Color(0XFFF2F2F6);
   static const Color scaffoldBackgroundColorDark = Colors.black;
   static const Color appBarBackgroundColorDark = Colors.black;
   static const Color disabledColorLight = Colors.white;
@@ -18,4 +20,13 @@ class ManagerColors {
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static const Color transparent = Colors.transparent;
+  static const Color redColor = Color(0XFFEB4D3D);
+  static const Color yellowColor = Color(0XFFF7CE45);
+  static Color yellowColor_30 = const Color(0XFFF7CE45).withOpacity(0.30);
+
+  static const Color greyColorIcon = Color(0XFF8E8E92);
+  static const Color blueColor = Color(0XFF4E94F8);
+
+  static const Color c13 = Color.fromRGBO(0, 0, 0, 0.3);
+
 }
