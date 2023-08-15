91ef16bdfd36f9a97a003733e531aa9195d41ce8
Khaled <khaled.shbair12@gmail.com>
2023-08-15 21:49:51 +0300

    edit dependency_injection.dart

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,4 +1,6 @@
+import '../core/storage/database/database_provider.dart';
 import '../core/storage/local/shared_preferences/shared_preferences_controller.dart';
+import '../features/note/presentation/controller/home_controller.dart';
 import '../features/on_boarding/presentation/controller/on_boarding_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,6 +8,7 @@ import 'package:get/get.dart';
 Future<void> initModule() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferencesController.initSharedPreferences();
+  await DatabaseProvider.initDatabase();
 }
 
 initOnBoarding() {
@@ -14,4 +17,13 @@ initOnBoarding() {
 
 disposeOnBoarding() {
   Get.delete<OnBoardingController>();
+}
+
+initHome() {
+  disposeOnBoarding();
+  Get.put<HomeController>(HomeController());
+}
+
+disposeHome() {
+  Get.delete<HomeController>();
 }
