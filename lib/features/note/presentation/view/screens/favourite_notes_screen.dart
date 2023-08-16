27fe687d72a0a31565af6e062857475718f63333
Khaled <khaled.shbair12@gmail.com>
2023-08-16 03:45:09 +0300

    edit FavouriteNotesScreen

--- lib/features/note/presentation/view/screens/favourite_notes_screen.dart
+++ lib/features/note/presentation/view/screens/favourite_notes_screen.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import '../../../../../core/widgets/my_scaffold_app.dart';
-import '../../controller/home_controller.dart';
 
 class FavouriteNotesScreen extends StatelessWidget {
   const FavouriteNotesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<HomeController>(
-      builder: (controller) {
-        return MyScaffoldApp(
-          controller: controller.searchController,
-          isFavouritesScreen: true,
-          onChanged: (value) => controller.searchNote(value),
-          list: controller.searchNotes,
-          currentDate: controller.currentDate,
-          loading: controller.loading,
-        );
-      },
+    return const MyScaffoldApp(
+      isFavouritesScreen: true,
     );
   }
 }
