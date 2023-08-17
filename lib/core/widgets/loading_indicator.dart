5669622b81f354ad02f249c79b3c381ef0806302
Khaled <khaled.shbair12@gmail.com>
2023-08-17 16:51:08 +0300

    create loading_indicator.dart

---
+++ lib/core/widgets/loading_indicator.dart
@@ -0,0 +1,12 @@
+import '/config/all_imports.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
